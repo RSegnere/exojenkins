2b18d9d117b1622a4935ffa05dba9758cde9dd4c
RSegnere <110993703+RSegnere@users.noreply.github.com>
2022-09-12 16:37:27 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello, World ! Nous sommes lundi") ;
+		System.out.println("Hello, World ! Nous sommes lundi et c'est super") ;
 	}
 }
